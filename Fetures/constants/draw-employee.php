7bd097a42182ec785fc16083b4b186e334a4ecae
Aazwd20 <asiflzwd@gmail.com>
2023-03-20 14:19:29 +0600

    draw employee first

---
+++ Fetures/constants/draw-employee.php
@@ -0,0 +1,11 @@
+<form name="frm" action="app/create-account.php" method="POST" autocomplete="off">
+<?php require 'check_reply.php'; ?>	
+<div class="login-box-wrapper">
+		
+<div class="modal-header">
+<h4 class="modal-title text-center">Create Your Account Today</h4>
+</div>
+
+<div class="modal-body">
+																
+<div class="row gap-20">
