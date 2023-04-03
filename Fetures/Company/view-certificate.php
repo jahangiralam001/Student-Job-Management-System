cb34b6d62663f0536d80e6a8091ebf17bb18ba2b
Aazwd20 <asiflzwd@gmail.com>
2023-04-03 15:51:47 +0600

    view_certificate complete

--- Fetures/Company/view-certificate.php
+++ Fetures/Company/view-certificate.php
@@ -10,7 +10,40 @@
 </head>
 
 <body>
+<?php
+require '../constants/db_config.php';
+$file_id = $_GET['id'];
 
+try {
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	
+$stmt = $conn->prepare("SELECT * FROM tbl_professional_qualification WHERE id = :fileid");
+$stmt->bindParam(':fileid', $file_id);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+foreach($result as $row)
+{
+    $certificate = $row['certificate'];
+	
+	?>
+	<div style="width:100%">
+    <iframe  style="border:none;" src="../ViewerJS/?title=CERTIFICATE#<?php echo 'data:application/pdf;base64,'.base64_encode($certificate).'' ?>" height="100%" width="100%"></iframe>
+
+    </div>
+
+<?php
+}
+
+					  
+}catch(PDOException $e)
+{
+
+}
+
+?>
 </body>
 
 </html>
